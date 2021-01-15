e83ac04e2d72d375d330b17acbbdcacef48a9660
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2021-01-15 07:47:24 +0000

    Create/Edit/Delete/List commands for the mysql/postgresl

--- watcher/api/common/interface.go
+++ watcher/api/common/interface.go
@@ -6,10 +6,17 @@ type Database interface {
 	Drop(name string) error
 }
 
+type User interface {
+	List() ([]string, error)
+	Create(name, password string) error
+	Delete(name string) error
+	Edit(name, password string) error
+}
+
 type Session interface {
 	String() string
 	SetupDDL() error
 	RunQueries(delay Delay, duration Duration)
-
 	GetDatabase() Database
+	GetUser() User
 }
---
+++ watcher/api/postgres/user.go
@@ -0,0 +1,94 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v4"
+)
+
+type User struct {
+	session *Session
+}
+
+//var ProtectedUsers = []string{
+//	"postgres",
+//	"standby",
+//	"cloudmanaged",
+//}
+
+func (usr *User) Create(name, password string) error {
+	ctx := context.TODO()
+	conn, err := pgx.Connect(ctx, usr.session.ConnectionString(usr.session.MasterIP, "postgres"))
+	if err != nil {
+		return err
+	}
+	defer conn.Close(ctx)
+
+	query := fmt.Sprintf(`
+CREATE USER %s WITH ENCRYPTED PASSWORD '%s';
+ALTER USER %s WITH SUPERUSER;
+`, name, password, name)
+	_, err = conn.Exec(ctx, query)
+	return err
+}
+
+func (usr *User) Delete(name string) error {
+	ctx := context.TODO()
+	conn, err := pgx.Connect(ctx, usr.session.ConnectionString(usr.session.MasterIP, "postgres"))
+	if err != nil {
+		return err
+	}
+	defer conn.Close(ctx)
+
+	query := fmt.Sprintf(`
+DROP USER %s;
+`, name)
+	_, err = conn.Exec(ctx, query)
+	return err
+}
+
+func (usr *User) Edit(name, password string) error {
+	ctx := context.TODO()
+	conn, err := pgx.Connect(ctx, usr.session.ConnectionString(usr.session.MasterIP, "postgres"))
+	if err != nil {
+		return err
+	}
+	defer conn.Close(ctx)
+
+	query := fmt.Sprintf(`
+ALTER USER %s WITH PASSWORD '%s';
+`, name, password)
+	_, err = conn.Exec(ctx, query)
+	return err
+}
+
+func (usr *User) List() ([]string, error) {
+	var items []string
+
+	ctx := context.TODO()
+	conn, err := pgx.Connect(ctx, usr.session.ConnectionString(usr.session.MasterIP, "postgres"))
+	if err != nil {
+		return items, err
+	}
+	defer conn.Close(ctx)
+
+	rows, err := conn.Query(ctx, `
+SELECT usename
+FROM pg_catalog.pg_user;
+`)
+	if err != nil {
+		return items, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var name string
+		err = rows.Scan(&name)
+		if err != nil {
+			return items, err
+		}
+		items = append(items, name)
+	}
+
+	return items, nil
+}
