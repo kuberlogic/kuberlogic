eda1fd0b838103e5d33e43d7ffaeb66984cb03cd
Arsenii Pastushenko <arsenii.pastushenko@protonmail.com>
2021-05-14 15:09:22 +0300

    Send user notifications for kuberlogicalerts

--- modules/operator/controllers/kuberlogicalert_controller.go
+++ modules/operator/controllers/kuberlogicalert_controller.go
@@ -2,17 +2,15 @@ package controllers
 
 import (
 	"context"
-	"fmt"
 	"github.com/go-logr/logr"
 	kuberlogicv1 "github.com/kuberlogic/operator/modules/operator/api/v1"
 	"github.com/kuberlogic/operator/modules/operator/util"
-	v1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sync"
 )
 
@@ -24,6 +22,10 @@ type KuberLogicAlertReconciler struct {
 	mu     sync.Mutex
 }
 
+const (
+	klaFinalizer = kuberlogicv1.Group + "/alert-finalizer"
+)
+
 // +kubebuilder:rbac:groups=cloudlinux.com,resources=kuberlogicalerts,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=cloudlinux.com,resources=kuberlogicalerts/status,verbs=get;update;patch
 func (r *KuberLogicAlertReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
@@ -31,13 +33,14 @@ func (r *KuberLogicAlertReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 
 	defer util.HandlePanic(log)
 
-	r.mu.Lock()
-	defer r.mu.Unlock()
+	mu := getMutex(req.NamespacedName)
+	mu.Lock()
+	defer mu.Unlock()
 
+	log.Info("reconciliation started")
 	// Fetch the KuberLogicAlert instance
-	klAlert := &kuberlogicv1.KuberLogicAlert{}
-	err := r.Get(ctx, req.NamespacedName, klAlert)
-	if err != nil {
+	kla := &kuberlogicv1.KuberLogicAlert{}
+	if err := r.Get(ctx, req.NamespacedName, kla); err != nil {
 		if k8serrors.IsNotFound(err) {
 			// Request object not found, could have been deleted after reconcile request.
 			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
@@ -49,102 +52,73 @@ func (r *KuberLogicAlertReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 		log.Error(err, "Failed to get KuberLogicAlert")
 		return ctrl.Result{}, err
 	}
-
-	// Trigger alert processing and update status if needed.
-	// This will trigger reconcilation loop one more time.
-	newStatus := r.Process(klAlert)
-	if !klAlert.IsEqual(newStatus) {
-		klAlert.SetStatus(newStatus)
-
-		err := r.Update(ctx, klAlert)
-		if err != nil {
-			log.Error(err, "Failed to update alert status")
+	// fetch the cluster information
+	kls := &kuberlogicv1.KuberLogicService{}
+	if err := r.Get(ctx, types.NamespacedName{Name: kla.Spec.Cluster, Namespace: req.Namespace}, kls); err != nil {
+		if k8serrors.IsNotFound(err) {
+			log.Info(req.Namespace, kla.Spec.Cluster, " kuberlogicservice not found")
 			return ctrl.Result{}, err
-		} else {
-			log.Info("Alert status is updated", "Status", klAlert.Status.Status)
+		}
+		log.Error(err, "failed to get kuberlogicservice")
+		return ctrl.Result{}, err
+	}
+	log = log.WithValues("kuberlogicservice", kls.Name)
+	log.Info("kuberlogicservice is found")
+
+	if kla.DeletionTimestamp != nil {
+		log.Info("kuberlogicalert is pending for deletion")
+		if controllerutil.ContainsFinalizer(kla, klaFinalizer) {
+			if err := r.finalize(ctx, kla, kls, log); err != nil {
+				log.Error(err, "error finalizing kuberlogicalert")
+				return ctrl.Result{}, err
+			}
+			controllerutil.RemoveFinalizer(kla, klaFinalizer)
+			if err := r.Update(ctx, kla); err != nil {
+				return ctrl.Result{}, err
+			}
+			return ctrl.Result{}, nil
 		}
 	}
 
+	if !controllerutil.ContainsFinalizer(kla, klaFinalizer) {
+		log.Info("adding finalizer", "finalizer", klaFinalizer)
+		controllerutil.AddFinalizer(kla, klaFinalizer)
+		err := r.Update(ctx, kla)
+		if err != nil {
+			log.Error(err, "error adding finalizer")
+		}
+		return ctrl.Result{}, err
+	}
+
+	// check if we need to send a notification about new alert
+	// only email notifications are supported
+	if ignore := kla.IsSilenced() || kla.IsNotificationSent(); !ignore {
+		if err := kla.NotifyNew(kls.GetAlertEmail()); err != nil {
+			log.Error(err, "notification sending failure")
+			return ctrl.Result{}, err
+		}
+
+		log.Info("alert notification sent", "address", kls.GetAlertEmail())
+		kla.NotificationSent(kls.GetAlertEmail())
+		if err := r.Status().Update(ctx, kla); err != nil {
+			return ctrl.Result{}, err
+		}
+	} else {
+		log.Info("notification conditions are not sent, skipping", "silenced", kla.IsSilenced(), "alreadySent", kla.IsNotificationSent())
+	}
 	return ctrl.Result{}, nil
 }
 
-func (r *KuberLogicAlertReconciler) Process(cla *kuberlogicv1.KuberLogicAlert) (status string) {
-	log := r.Log.WithValues("kuberlogicalert", cla.Name)
-	log.Info("Running alert processing")
-
-	switch cla.Status.Status {
-	case "":
-		return NewAlertProcess(r, cla, log)
-	case kuberlogicv1.AlertCreatedStatus:
-		return CreatedAlertProcess(r, cla, log)
-	case kuberlogicv1.AlertAckedStatus:
-		return AckedAlertProcess(r, cla, log)
-	case kuberlogicv1.AlertResolvedStatus:
-		return kuberlogicv1.AlertResolvedStatus
-	default:
-		return kuberlogicv1.AlertUnknownStatus
-	}
+// finalize function "resolves" an alert when kuberlogicalert is deleted.
+func (r *KuberLogicAlertReconciler) finalize(ctx context.Context, kla *kuberlogicv1.KuberLogicAlert, kls *kuberlogicv1.KuberLogicService, log logr.Logger) error {
+	log.Info("processing finalizer")
+	err := kla.NotifyResolved(kls.GetAlertEmail())
+	log.Info("alert recovery notification sent", "address", kls.GetAlertEmail())
+	return err
 }
 
 func (r *KuberLogicAlertReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&kuberlogicv1.KuberLogicAlert{}).
 		Complete(r)
-}
-
-var AlertsDB = map[string]func(r *KuberLogicAlertReconciler, cla *kuberlogicv1.KuberLogicAlert) error{
-	"kuberlogic-memory-usage": func(r *KuberLogicAlertReconciler, cla *kuberlogicv1.KuberLogicAlert) error {
-		// just increases memory limits
-
-		log := r.Log.WithValues("kuberlogicalert", cla.Name)
-
-		cm := &kuberlogicv1.KuberLogicService{}
-		if err := r.Get(context.Background(), types.NamespacedName{
-			Name:      cla.Spec.Cluster,
-			Namespace: cla.Namespace,
-		}, cm); err != nil {
-			log.Error(err, "Error getting related kuberlogic cluster")
-			return err
-		}
-
-		// calculate new memory limits
-		// this is just an example so implementation is very dumb
-		cm.Spec.Resources.Limits = v1.ResourceList{
-			v1.ResourceCPU:    cm.Spec.Resources.Limits.Cpu().DeepCopy(),
-			v1.ResourceMemory: resource.MustParse(fmt.Sprintf("%dKi", cm.Spec.Resources.Limits.Memory().Value()*2/1024)),
-		}
-		if err := r.Update(context.Background(), cm); err != nil {
-			log.Error(err, "Error updating resources for related KuberLogicService object!")
-			return err
-		}
-		return nil
-	},
-}
-
-func NewAlertProcess(r *KuberLogicAlertReconciler, cla *kuberlogicv1.KuberLogicAlert, log logr.Logger) (status string) {
-	log.Info("Received an alert")
-	return kuberlogicv1.AlertCreatedStatus
-}
-
-func CreatedAlertProcess(r *KuberLogicAlertReconciler, cla *kuberlogicv1.KuberLogicAlert, log logr.Logger) (status string) {
-	log.Info("Alert is valid and is ready to be processed")
-	return kuberlogicv1.AlertAckedStatus
-}
-
-func AckedAlertProcess(r *KuberLogicAlertReconciler, cla *kuberlogicv1.KuberLogicAlert, log logr.Logger) (status string) {
-	log.Info("Processing alert")
-
-	alertAction := AlertsDB[cla.Spec.AlertName]
-	if alertAction == nil {
-		log.Info("No meaningful action found", "alert type", cla.Spec.AlertName)
-		return kuberlogicv1.AlertUnknownStatus
-	} else {
-		log.Info("Found an action for alert")
-	}
-	if err := alertAction(r, cla); err != nil {
-		log.Error(err, "Error processing action for alert", "alert type", cla.Spec.AlertName)
-		return kuberlogicv1.AlertUnknownStatus
-	}
-
-	return kuberlogicv1.AlertResolvedStatus
 }
