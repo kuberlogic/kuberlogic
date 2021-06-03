cd5bcc565d4850b2ae97735368fe9580cfb2f31a
Arsenii Pastushenko <arsenii.pastushenko@protonmail.com>
2021-06-03 16:42:13 +0300

    Use another logic for keycloak installation

--- modules/apiserver/tests/config/wait-for-keycloak.sh
+++ modules/apiserver/tests/config/wait-for-keycloak.sh
@@ -2,6 +2,7 @@
 SLEEP_TIME=30
 for i in $(seq 1 10); do
   kubectl get keycloakclient -o jsonpath='{.items[0].status.ready}' | grep true && exit 0
+  sleep $SLEEP_TIME
 done
 echo "Keycloak is not ready in time!"
 exit 1
