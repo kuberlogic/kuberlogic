04f59323fe9de529cef423f283dc7bd5cfe11089
Arsenii Pastushenko <arsenii.pastushenko@protonmail.com>
2021-06-17 15:53:03 +0300

    Show response body on error

--- modules/apiserver/tests/impl_test.go
+++ modules/apiserver/tests/impl_test.go
@@ -121,6 +121,8 @@ func (a *API) sendRequestTo(method, endpoint string) {
 
 func (a *API) responseCodeShouldBe(code int) {
 	if code != a.response.StatusCode {
+		a.encodeResponseToJson()
+		log.Error(fmt.Sprintf("response body: %s", a.jsonResponse))
 		a.t.Errorf("expected response code to be: %d, but actual is: %d", code, a.response.StatusCode)
 	}
 }
