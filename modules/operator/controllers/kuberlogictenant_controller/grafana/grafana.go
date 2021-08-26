87883bc00e4714b49ef997a3ee1b4d2ef13e6166
Arsenii Pastushenko <arsenii.pastushenko@protonmail.com>
2021-08-26 15:19:02 +0300

    Reconcile Grafana dashboards

--- modules/operator/controllers/kuberlogictenant_controller/grafana/grafana.go
+++ modules/operator/controllers/kuberlogictenant_controller/grafana/grafana.go
@@ -34,14 +34,9 @@ func (gr *grafana) Sync() error {
 	if err := gr.ensureUser(gr.kt.Spec.OwnerEmail, "", uuid.New().String(), VIEWER_ROLE, orgId); err != nil {
 		return fmt.Errorf("error creating grafana viewer user: %v", err)
 	}
-	// ensure that there is a user with Editor role for this organization
-	// this user is used to create/update tenant dashboards
-	editorUsername := gr.kt.Name + "-editor"
-	if err := gr.ensureUser("", editorUsername, uuid.New().String(), EDITOR_ROLE, orgId); err != nil {
-		return fmt.Errorf("error creating grafana editor user: %v", err)
-	}
-	if err := gr.ensureDashboards(editorUsername, gr.kt.Status.Services, orgId); err != nil {
-		return err
+	// create Grafana dashboards
+	if err := gr.ensureDashboards(gr.kt.Status.Services, orgId); err != nil {
+		return fmt.Errorf("error creating grafana dashboards: %v", err)
 	}
 	return nil
 }
