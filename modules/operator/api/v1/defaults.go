5fd00ae9b1927cf0bfa4e799e0d90137c59127ee
Arsenii Pastushenko <arsenii.pastushenko@protonmail.com>
2021-10-01 10:27:59 +0300

    Set minimum memory limits to 0.5G

--- modules/operator/api/v1/defaults.go
+++ modules/operator/api/v1/defaults.go
@@ -24,6 +24,6 @@ var DefaultResources = v1.ResourceRequirements{
 		// CPU 250m required minimum for zalando/posgtresql
 		// Memory 250Mi required minimum for zalando/posgtresql
 		v1.ResourceCPU:    resource.MustParse("250m"),
-		v1.ResourceMemory: resource.MustParse("512M"),
+		v1.ResourceMemory: resource.MustParse("500M"),
 	},
 }
