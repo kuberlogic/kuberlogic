6eeae7da0e6b44e38df32cac66da9044d503d9c2
Arsenii Pastushenko <arsenii.pastushenko@protonmail.com>
2021-10-07 14:12:28 +0300

    Add copyright headers and LICENSE

--- modules/agent/command/command.go
+++ modules/agent/command/command.go
@@ -1,3 +1,19 @@
+/*
+ * CloudLinux Software Inc 2019-2021 All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package command
 
 import (
@@ -17,9 +33,9 @@ const (
 )
 
 type Command struct {
-	Name string
+	Name    string
 	Command string
-	Args []string
+	Args    []string
 }
 
 type Queue struct {
