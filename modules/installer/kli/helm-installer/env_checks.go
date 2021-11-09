c387b22c9b2d1aaba1d59cf6aef6969c7694a5b8
Arsenii Pastushenko <arsenii.pastushenko@protonmail.com>
2021-11-09 17:53:26 +0200

    Wait 2 minutes until the LoadBalancer receives Ingress IP

--- modules/installer/kli/helm-installer/env_checks.go
+++ modules/installer/kli/helm-installer/env_checks.go
@@ -116,7 +116,7 @@ func checkLoadBalancerServiceType(clientset kubernetes.Interface, log logger.Log
 	const (
 		testServiceName = "kubernetes-test-service"
 		testNamespace   = "default"
-		waitTimeoutSec  = 30
+		waitTimeoutSec  = 120
 	)
 	log.Infof("Creating test service %s/%s", testServiceName, testNamespace)
 	svc, err := createTestLoadBalancer(testServiceName, testNamespace, clientset)
