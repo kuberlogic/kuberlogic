2ab31551646c5ca4ea2c76b9158f098764c57433
Arsenii Pastushenko <arsenii.pastushenko@protonmail.com>
2021-11-26 15:09:52 +0200

    Use sudo for placing kuberlogic-installer to /usr/local/bin

--- modules/installer/installer.sh
+++ modules/installer/installer.sh
@@ -16,9 +16,9 @@ main() {
 
   echo "Detecting os: " $platform
 
-  curl -L -o /usr/local/bin/kuberlogic-installer \
+  sudo curl -L -o /usr/local/bin/kuberlogic-installer \
     https://github.com/kuberlogic/kuberlogic/releases/download/$tag/kuberlogic-installer-$platform-amd64
-  chmod +x /usr/local/bin/kuberlogic-installer
+  sudo chmod +x /usr/local/bin/kuberlogic-installer
   /usr/local/bin/kuberlogic-installer install all
 }
 
