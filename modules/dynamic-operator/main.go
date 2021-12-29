f9acb598fc4409f5cb5a945dab4ee2adeee0f06d
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2021-12-29 11:10:09 +0300

    Webhook setup for KuberLogicService

--- modules/dynamic-operator/main.go
+++ modules/dynamic-operator/main.go
@@ -93,6 +93,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "KuberLogicServiceType")
 		os.Exit(1)
 	}
+	if err = (&kuberlogiccomv1alpha1.KuberLogicService{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "KuberLogicService")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
