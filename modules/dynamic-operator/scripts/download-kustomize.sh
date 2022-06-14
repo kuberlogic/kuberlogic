b5b6312a563f92a2c1bf4a825ed23b6b68b09a07
Telepenin Nikolay <ntelepenin@cloudlinux.com>
2022-06-14 13:00:03 +0300

    Downloading kustomize binary to prevent using go when kuberlogic is deploying (#148)
    * Downloading kustomize binary to prevent using go when kuberlogic is deploying
    
    * End space in the bash script
    
    * Formatting bash script

---
+++ modules/dynamic-operator/scripts/download-kustomize.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#
+# CloudLinux Software Inc 2019-2021 All Rights Reserved
+#
+
+set -eou pipefail
+
+function get_system {
+  echo $(uname -s | tr '[:upper:]' '[:lower:]')
+}
+
+function get_arch {
+  case "$(uname -m)" in
+  x86_64 | amd64)
+    echo amd64
+    ;;
+  i?86)
+    echo s390x
+    ;;
+  arm*)
+    echo arm64
+    ;;
+  powerpc | ppc64)
+    echo ppc64le
+    ;;
+  *)
+    echo "$(uname -m) is not supported" >/dev/tty
+    exit 1
+    ;;
+  esac
+}
+
+function main {
+  version=$1
+  target=$2
+  if [ ! -f "$target" ]; then
+    parentdir=$(dirname $target)
+
+    if [ ! -d "$parentdir" ]; then
+      echo "$parentdir" >/dev/tty
+      mkdir -p $parentdir
+    fi
+
+    tmpdir=$(mktemp -d)
+    archive="$tmpdir/kustomize.tar.gz"
+    url="https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2F${version}/kustomize_${version}_$(get_system)_$(get_arch).tar.gz"
+    echo "Downloading kustomize $1" >/dev/tty
+    curl -o $archive -L $url
+
+    echo "Extracting archive to $2" >/dev/tty
+    tar -xvf $archive -C $parentdir
+
+    rm -rf $tmpdir
+    echo "Done."
+  else
+    echo "$(basename $target) file already exists"
+  fi
+}
+
+[ $# -eq 0 ] && {
+  echo "Usage: $0 <version> <target>"
+  exit 1
+}
+
+main $@
