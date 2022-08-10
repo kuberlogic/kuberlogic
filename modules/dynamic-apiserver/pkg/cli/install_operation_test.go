bfe040ce1d483f369b4b8616166b28727c1c2a33
Arsenii Pastushenko <arsenii.pastushenko@protonmail.com>
2022-08-10 14:28:28 +0300

    Add tests

--- modules/dynamic-apiserver/pkg/cli/install_operation_test.go
+++ modules/dynamic-apiserver/pkg/cli/install_operation_test.go
@@ -3,11 +3,51 @@ package cli
 import (
 	"github.com/spf13/viper"
 	corev1 "k8s.io/api/core/v1"
+	v12 "k8s.io/api/networking/v1"
+	v1 "k8s.io/api/storage/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	k8stesting "k8s.io/client-go/kubernetes/fake"
 	"os"
 	"path/filepath"
 	"testing"
+)
+
+var (
+	fakeClusterResources = []runtime.Object{
+		&corev1.Service{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "kls-api-server",
+				Namespace: "kuberlogic",
+			},
+			Spec: corev1.ServiceSpec{
+				Ports: []corev1.ServicePort{
+					{
+						Port: 80,
+					},
+				},
+			},
+			Status: corev1.ServiceStatus{
+				LoadBalancer: corev1.LoadBalancerStatus{
+					Ingress: []corev1.LoadBalancerIngress{
+						{
+							IP: "127.0.0.1",
+						},
+					},
+				},
+			},
+		},
+		&v1.StorageClass{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "demo",
+			},
+		},
+		&v12.IngressClass{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "demo",
+			},
+		},
+	}
 )
 
 func TestInstallLB(t *testing.T) {
@@ -19,32 +59,11 @@ func TestInstallLB(t *testing.T) {
 	kustomizeBin = "echo"
 	viper.SetConfigFile(configFile)
 
-	cmd, err := MakeRootCmd(nil, k8stesting.NewSimpleClientset(&corev1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "kls-api-server",
-			Namespace: "kuberlogic",
-		},
-		Spec: corev1.ServiceSpec{
-			Ports: []corev1.ServicePort{
-				{
-					Port: 80,
-				},
-			},
-		},
-		Status: corev1.ServiceStatus{
-			LoadBalancer: corev1.LoadBalancerStatus{
-				Ingress: []corev1.LoadBalancerIngress{
-					{
-						IP: "127.0.0.1",
-					},
-				},
-			},
-		},
-	}))
+	cmd, err := MakeRootCmd(nil, k8stesting.NewSimpleClientset(fakeClusterResources...))
 	if err != nil {
 		t.Fatal(err)
 	}
-	cmd.SetArgs([]string{"install", "--non-interactive"})
+	cmd.SetArgs([]string{"install", "--non-interactive", "--storage_class", "demo", "--ingress_class", "demo"})
 	if err := cmd.Execute(); err != nil {
 		t.Fatal(err)
 	}
@@ -67,12 +86,31 @@ func TestInstallClusterNotAvailable(t *testing.T) {
 	kustomizeBin = "exit 1"
 	viper.SetConfigFile(configFile)
 
-	cmd, err := MakeRootCmd(nil, k8stesting.NewSimpleClientset())
+	cmd, err := MakeRootCmd(nil, k8stesting.NewSimpleClientset(fakeClusterResources...))
 	if err != nil {
 		t.Fatal(err)
 	}
-	cmd.SetArgs([]string{"install", "--non-interactive"})
+	cmd.SetArgs([]string{"install", "--non-interactive", "--storage_class", "demo", "--ingress_class", "demo"})
 	if err := cmd.Execute(); err.Error() != "Kubernetes is not available via kubectl" {
+		t.Fatal(err)
+	}
+}
+
+func TestIngressClassNotAvailable(t *testing.T) {
+	configDir, _ := os.MkdirTemp("", "install-test")
+	defer os.RemoveAll(configDir)
+
+	configFile = filepath.Join(configDir, "config.yaml")
+	kubectlBin = "exit 1"
+	kustomizeBin = "exit 1"
+	viper.SetConfigFile(configFile)
+
+	cmd, err := MakeRootCmd(nil, k8stesting.NewSimpleClientset(fakeClusterResources...))
+	if err != nil {
+		t.Fatal(err)
+	}
+	cmd.SetArgs([]string{"install", "--non-interactive", "--storage_class", "demo", "--ingress_class", "fake"})
+	if err := cmd.Execute(); err.Error() != "error processing ingress_class flag: fake is not available. Available: demo" {
 		t.Fatal(err)
 	}
 }
