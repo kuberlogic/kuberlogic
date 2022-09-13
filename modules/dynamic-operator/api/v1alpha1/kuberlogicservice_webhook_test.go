3213a627cb9bc86779c99a1ef75038a9bfd9e44e
Arsenii Pastushenko <arsenii.pastushenko@protonmail.com>
2022-09-13 12:46:19 +0300

    Rebase master

--- modules/dynamic-operator/api/v1alpha1/kuberlogicservice_webhook_test.go
+++ modules/dynamic-operator/api/v1alpha1/kuberlogicservice_webhook_test.go
@@ -5,8 +5,6 @@
 package v1alpha1
 
 import (
-	"time"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	v1 "k8s.io/api/core/v1"
@@ -14,6 +12,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"time"
 )
 
 var _ = Describe("KuberlogicService controller", func() {
